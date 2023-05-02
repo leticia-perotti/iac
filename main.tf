a8692ab9090f525ef0a76ae79a2d643db596e4fd
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-05-02 20:49:57 -0300

    ansible

--- main.tf
+++ main.tf
@@ -15,10 +15,30 @@ provider "aws" {
 } // provedor utilizado 
 
 resource "aws_instance" "app_server" {
-    ami = "ami-830c94e3" //tem que pegar do console da AWS ou do provedor
+    ami = "ami-830c94e3" #tem que pegar do console da AWS ou do provedor
     instance_type = "t2.micro"
 
+    key_name = "iac-alura" #nome da chave
+
+    #user_data = <<-EOF
+    #    #!/bin/bash 
+    #    cd /home/ubuntu 
+    #    echo "<h1>Feito com o terraform</h1>" > index.html 
+    #    nohup busybox hhtpd -f -p 8080 &
+    #               EOF
+    #               código bash para configurar o servidor automaticamente
+
     tags = {
-        Name = "Primeira instância"
+        Name = "Teste da AWS"
     }
 }
+
+#Passo 1: cria terraform 
+#Passo 2: Cria a máquina virtual 
+#Passo 3: configura as chaves 
+#Passo 4: Tem que habilitar o trafego de entrada e saída da máquina
+
+#Rodar servidor http do ubunto -> nohup: busybox hhtpd -f -p 8080 &
+#O comando levanta um servidor HHTP na porta 8080, que não será fechado quando desconectar o SSH para
+
+#Tem que por gitignore no ftstate e chave ssh
